8223c4b27674e2c7b472ca9d8e2ec3dd116fb442
AYMAN HOUADRIA <al286342@uji.es>
2023-06-11 21:20:30 +0200

    add redux and create product api

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const  express =  require("express");
 const  cors =  require("cors");
+const products = require("./products")
 
 const app =  express();
 
@@ -8,6 +9,9 @@ app.use(cors());
 
 app.get("/", (req, res) =>{
     res.send("Welcome")
+});
+app.get("/products", (req, res) =>{
+    res.send(products)
 });
 const port = process.env.PORT || 5000;
 app.listen(5000, console.log(`Server running on port ${port}`));
---
+++ backend/products.js
@@ -0,0 +1,26 @@
+const products = [
+    {
+        id: 1,
+        name: "IPhone 12 Pro Max",
+        desc: "IPhoine de 6 pulgadas ",
+        price: 1469,
+        image:"https://res.cloudinary.com/dzcuylgug/image/upload/v1686509508/pngwing.com_ovrmpu.png",
+    },
+    {
+        id: 2,
+        name: "New Balance 660n",
+        desc: "New Balance 660n ",
+        price: 146,
+        image:"https://res.cloudinary.com/dzcuylgug/image/upload/v1686492440/My%20online%20shop%20demo/pngegg_yj1nhl.png",
+    },
+    {
+        id: 3,
+        name: "Logitech 30G",
+        desc: "Logitech mouse ",
+        price: 69,
+        image:"https://res.cloudinary.com/dzcuylgug/image/upload/v1686492124/cld-sample-5.jpg",
+    },
+
+];
+
+module.exports = products;
