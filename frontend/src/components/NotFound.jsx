1010577a5e5a6c09b5f92e1af95d283540d9e5e8
AYMAN HOUADRIA <al286342@uji.es>
2023-06-11 02:30:40 +0200

    backend and frontend structure creation

---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+    return (  
+        <div className="not-found">
+            <h2>404</h2>
+            <p> Page not found</p>
+        </div>
+    );
+}
+ 
+export default NotFound;
+<div className="not-found">
+    <h2>404</h2>
+    <p> Page not found</p>
+</div>
